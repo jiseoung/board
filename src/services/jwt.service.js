98276657b9fea47ba6a0a3001d6d19a29d2c1d9b
jiseoung <misojskim@naver.com>
2024-11-05 16:59:50 +0900

    build: jwt class -> jwt functions

--- src/services/jwt.service.js
+++ src/services/jwt.service.js
@@ -1,29 +1,28 @@
 const jwt = require('jsonwebtoken');
 require("dotenv").config({ path: __dirname + "/src/config/.env" });
 
-const jwt_key = process.env.JWT_SERCRET_KEY
+const jwt_key = process.env.JWT_SERCRET_KEY;
 
-class JWT {
-    generate_jwt(payload) {
-        const token = jwt.sign(payload, jwt_key, { algorithm: "HS256", expiresIn: '24h' });
+
+exports.generate_jwt = async (payload) => {
+    const token = jwt.sign(payload, jwt_key, { algorithm: "HS256", expiresIn: '24h' });
         
-        return token;
-    };
+    return token;
+};
 
-    verify_jwt(token) {
-        try {
-            const data = jwt.decode(token, jwt_key);
+exports.verify_jwt = async (token) => {
+    try {
+        const data = jwt.decode(token, jwt_key);
 
-            return data;
-        } catch (e) {
-            if (e.message === "jwt expired") {
-                return "expired";
-            } else {
-                return "invalid";
-            }
+        return data;
+    } catch (e) {
+        if (e.message === "jwt expired") {
+            return "expired";
+        } else {
+            return "invalid";
         }
-        
     }
-}
+        
+};
 
-module.exports = JWT;
+
