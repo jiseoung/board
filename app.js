9ae49d1da80c9813994a03dd3af7a4e12ca2134c
jiseoung <misojskim@naver.com>
2024-11-05 10:36:11 +0900

    app

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+require("dotenv").config({ path: __dirname + "/src/config/.env" });
+
+const index_router = require(__dirname + '/src/controllers/index.controllers.js');
+const register_router = require(__dirname + '/src/controllers/user.controllers/register.controllers.js');
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, '/public/views'));
+
+app.use('/', index_router);
+app.use('/user', register_router);
+
+app.listen(port, () => {
+    console.log('hi');
+    console.log(port);
+});
