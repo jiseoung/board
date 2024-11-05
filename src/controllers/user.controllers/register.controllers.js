8961bbe7d075ac4b1253d838cce67143e7231e44
jiseoung <misojskim@naver.com>
2024-11-05 10:44:07 +0900

    feat: register page router

---
+++ src/controllers/user.controllers/register.controllers.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const user_register = require('../../services/register.service.js');
+
+const router = express.Router();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set('views', path.join(__dirname + '/public/views'));
+
+router.get('/register', (req, res) => {
+    res.render('register');
+});
+
+router.post('/register', (req, res) => {
+    var { id, pw, username, email } = req.body;
+
+    console.log('body : ' + id + pw + username + email);
+    var result = user_register(id, pw, username, email);
+    if (result) {
+        res.send('<script>alert("register ok");</script>');
+    }
+});
+
+module.exports = router;
