1422019cf5b2751e6addfea83abeabe85900e046
jiseoung <misojskim@naver.com>
2024-11-06 11:11:20 +0900

    fix: sync hash.create_hash func

--- src/services/user.service/login.service.js
+++ src/services/user.service/login.service.js
@@ -3,14 +3,14 @@ const hash = require('../../middlewares/hash.middleware.js');
 
 const user_login = async (id, pw) => {
     try {
-        const hased_pw = hash.create_hash(pw);
+        const hased_pw = await hash.create_hash(pw);
 
         const result = await connection.execute(
-            'SELECT * FROM users where `id`= ? and `pw`= ?',
-            [id, pw]
+            'SELECT username, role FROM users where id= ? and pw= ?',
+            [id, hased_pw]
         );
 
-        if (id === result[id] && hased_pw === result[pw]) {
+        if (id === result.id && hased_pw === result.pw) {
             return result;
         }
     } catch (e) {
