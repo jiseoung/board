c94547bc8db72f4cabf2e3899ff49ebcafa6ebd0
jiseoung <misojskim@naver.com>
2024-11-06 11:11:48 +0900

    fix: sync hash.create_hash func

--- src/services/user.service/register.service.js
+++ src/services/user.service/register.service.js
@@ -1,9 +1,9 @@
 const connection = require('../db_connect.service.js');
 const hash = require('../../middlewares/hash.middleware.js');
 
-const user_register = async(id, pw, username, email) => {
+const user_register = async (id, pw, username, email) => {
     try {
-        const hashed_pw = hash.create_hash(pw);
+        const hashed_pw = await hash.create_hash(pw);
 
         const result = await connection.execute(
             'INSERT into users (id, pw, username, email, role) values (?, ?, ?, ?, ?)',
