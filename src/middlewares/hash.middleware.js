ccc5d1cf6989c203fa90e5470e5b785bd813b43d
jiseoung <misojskim@naver.com>
2024-11-06 11:09:12 +0900

    fix: how to create hashed pw

--- src/middlewares/hash.middleware.js
+++ src/middlewares/hash.middleware.js
@@ -4,8 +4,7 @@ require("dotenv").config({ path: __dirname + "/src/config/.env" });
 const salt = process.env.SALT;
 
 exports.create_hash = async (pw) => {
-    const key = await crypto.pbkdf2Promise(pw, salt, 9812, 64, "sha512");
-    const hashed_pw = key.toString("base64");
+    const hashed_pw = await crypto.pbkdf2Sync(pw, salt, 2302, 64, 'sha512').toString('base64');
 
     return hashed_pw;
 }
