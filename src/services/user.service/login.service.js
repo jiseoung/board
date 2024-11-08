a2c4f641206fd3f8556b6ddb6ea57f088c215ccf
jiseoung <misojskim@naver.com>
2024-11-08 16:49:27 +0900

    fix: generate jwt token

--- src/services/user.service/login.service.js
+++ src/services/user.service/login.service.js
@@ -1,20 +1,27 @@
-const connection = require('../db_connect.service.js');
+const pool = require('../db_connect.service.js');
 const hash = require('../../middlewares/hash.middleware.js');
+const jwt = require('../../services/jwt.service.js');
 
 const user_login = async (id, pw) => {
+    const connection = await pool.getConnection(async conn => conn);
+
     try {
         const hased_pw = await hash.create_hash(pw);
 
-        const result = await connection.execute(
+        const [result] = await connection.execute(
             'SELECT username, role FROM users where id= ? and pw= ?',
             [id, hased_pw]
         );
 
-        if (id === result.id && hased_pw === result.pw) {
-            return result;
+        if (result) {
+            const jwt_token = jwt.generate_jwt({ username : result[0].username, role : result[0].role});
+            
+            return jwt_token;
         }
     } catch (e) {
-        console.log(e);
+        console.log('user_login error : ' + e);
+    } finally {
+        connection.release();
     }
 }
 
