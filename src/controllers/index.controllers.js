10e0c7107f9d888ff5528b3cbe65333458bac4c2
jiseoung <misojskim@naver.com>
2024-11-11 15:06:03 +0900

    build: parse cookie using cookie-parser

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+
+const jwt = require('../services/jwt.service.js');
 
 const router = express.Router();
 const app = express();
 
 app.set('views', path.join(__dirname + '/public/views'));
+router.use(cookieParser());
 
 router.get('/', (req, res) => {
-    res.render('index');
+    const jwt_token = req.cookies.jwt_token;
+
+    if (!jwt_token) {
+        res.render('index', { 'username' : null });
+    }
+    else {
+        const data = jwt.verify_jwt(jwt_token);
+
+        res.render('index', { 'username' : data.username , 'role' : data.role });
+    }
 })
 
 module.exports = router;
