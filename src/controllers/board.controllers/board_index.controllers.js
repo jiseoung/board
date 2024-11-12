438023968f9a1b234e43a194833060ca0f86c735
jiseoung <misojskim@naver.com>
2024-11-12 16:56:24 +0900

    feat: show board writes list

--- src/controllers/board.controllers/board_index.controllers.js
+++ src/controllers/board.controllers/board_index.controllers.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const path = require('path');
 
+const board_index = require('../../services/board.service/board_index.service.js');
+
 const app = express();
 const router = express.Router();
 
 app.set('views', path.join(__dirname + '/public/views'));
 
-router.get('/', (req, res) => {
-    res.render('board');
+router.get('/', async (req, res) => {
+    const writes_list = await board_index.show_writes_list();
+
+    res.render('board_index', { 'writes_list' : writes_list, 'index' : 0 });
 });
 
 module.exports = router;
