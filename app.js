3094654757c38b11f4c60862a249e1c3d0c024a3
jiseoung <misojskim@naver.com>
2024-11-15 11:05:13 +0900

    feat: user info router

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const index_router = require(__dirname + '/src/controllers/index.controllers.js'
 const register_router = require(__dirname + '/src/controllers/user.controllers/register.controllers.js');
 const login_router = require(__dirname + '/src/controllers/user.controllers/login.controllers.js');
 const logout_router = require(__dirname + '/src/controllers/user.controllers/logout.controllers.js');
+const info_router = require(__dirname + '/src/controllers/user.controllers/info.controllers.js');
+
 const board_index_router = require(__dirname + '/src/controllers/board.controllers/board_index.controllers.js');
 const board_write_router = require(__dirname + '/src/controllers/board.controllers/board_write.controllers.js');
+const board_show_router = require(__dirname + '/src/controllers/board.controllers/board_show.controllers.js');
 
 const app = express();
 const port = process.env.PORT;
@@ -22,8 +25,10 @@ app.use('/', index_router);
 app.use('/user', register_router);
 app.use('/user', login_router);
 app.use('/user', logout_router);
+app.use('/user', info_router);
 app.use('/board', board_index_router);
 app.use('/board', board_write_router);
+app.use('/board', board_show_router);
 
 app.listen(port, () => {
     console.log('hi');
