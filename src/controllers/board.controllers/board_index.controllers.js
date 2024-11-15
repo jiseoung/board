e0c881cabd7fb8d86a5c8ba9aa9093e990c56774
jiseoung <misojskim@naver.com>
2024-11-15 11:11:13 +0900

    feat: add logined variable to check admin

--- src/controllers/board.controllers/board_index.controllers.js
+++ src/controllers/board.controllers/board_index.controllers.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 
 const board_index = require('../../services/board.service/board_index.service.js');
+const jwt = require('../../services/jwt.service.js');
 
 const app = express();
 const router = express.Router();
 
 app.set('views', path.join(__dirname + '/public/views'));
+router.use(cookieParser());
 
 router.get('/', async (req, res) => {
+    const logined = jwt.verify_jwt(req.cookies.jwt_token).username;
+
     const writes_list = await board_index.show_writes_list();
 
-    res.render('board_index', { 'writes_list' : writes_list, 'index' : 0 });
+    res.render('board_index', { 'writes_list' : writes_list, 'logined' : logined });
 });
 
 module.exports = router;
