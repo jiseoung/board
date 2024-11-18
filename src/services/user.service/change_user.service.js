6cebdcb98baf767a62696e8b0e37df3e3d5ecb54
jiseoung <misojskim@naver.com>
2024-11-18 20:21:12 +0900

    feat: change user pw service

--- src/services/user.service/change_user.service.js
+++ src/services/user.service/change_user.service.js
@@ -1,6 +1,9 @@
 const pool = require('../db_connect.service.js');
 const jwt = require('../jwt.service.js');
 
+const hash = require('../../middlewares/hash.middleware.js');
+const match_pw = require('../../middlewares/match_pw.middleware.js');
+
 exports.change_info = async (id, username, email, before_username) => {
     const connection = await pool.getConnection(async conn => conn);
 
@@ -32,13 +35,32 @@ exports.change_info = async (id, username, email, before_username) => {
     }
 }
 
-exports.change_pw = async (req, res) => {
+exports.change_pw = async (current_pw, change_pw, username) => {
     const connection = await pool.getConnection(async conn => conn);
 
     try {
+        const is_match = await match_pw.check(current_pw, username);
 
+        if (is_match === 'match') {
+            const hashed_change_pw = await hash.create_hash(change_pw);
+
+            const change_pw_result = await connection.execute(
+                'UPDATE users SET pw = ? WHERE username = ?',
+                [hashed_change_pw, username]
+            );
+
+            if (change_pw_result) {
+                return 'change';
+            }
+            else {
+                return 'not_change';
+            }
+        }
+        else if (is_match === 'not_match') {
+            return 'not_change';
+        }
     } catch (e) {
-
+        console.log('change pw error : ' + e);
     } finally {
         connection.release();
     }
