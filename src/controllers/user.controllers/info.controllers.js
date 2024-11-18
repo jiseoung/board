b00315b99965f013fd0b2dc3b648413d17605f44
jiseoung <misojskim@naver.com>
2024-11-18 13:56:25 +0900

    feat: withdraw_user router

--- src/controllers/user.controllers/info.controllers.js
+++ src/controllers/user.controllers/info.controllers.js
@@ -9,16 +9,32 @@ const router = express.Router();
 const app = express();
 
 router.use(cookieParser());
+app.use(express.json());
 app.set('views', path.join(__dirname + '/public/views'));
 
 router.get('/info', async (req, res) => {
     const jwt_token = req.cookies.jwt_token;
-
     const username = await jwt.verify_jwt(jwt_token).username;
+
     if (username !== undefined) {
         const user = await user_info.parse_user(username);
         
         res.render('info', { 'user' : user });
+    }
+})
+
+router.delete('/info/withdraw_user', async (req, res) => {
+    const { answer } = req.body;
+    const jwt_token = req.cookies.jwt_token;
+    const username = await jwt.verify_jwt(jwt_token).username;
+
+    try {
+        if (answer) {
+            await user_info.withdraw_user(username);
+            res.clearCookie('jwt_token').send('ok');
+        }
+    } catch (e) {
+        console.log('/info/withdraw_user error : ' + e);
     }
 })
 
