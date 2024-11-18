cc111cc138d425e27075b86bd195058a6c7e33c0
jiseoung <misojskim@naver.com>
2024-11-18 17:28:16 +0900

    feat: change user info service

--- src/services/user.service/info.service.js
+++ src/services/user.service/info.service.js
@@ -1,4 +1,5 @@
 const pool = require('../db_connect.service');
+const jwt = require('../../services/jwt.service.js');
 
 exports.parse_user = async (username) => {
     const connection = await pool.getConnection(async conn => conn);
@@ -12,6 +13,37 @@ exports.parse_user = async (username) => {
         return user;
     } catch (e) {
         console.log('parse_user error : ' + e);
+    } finally {
+        connection.release();
+    }
+}
+
+exports.change_info = async (id, username, email, before_username) => {
+    const connection = await pool.getConnection(async conn => conn);
+
+    try {
+        await connection.execute(
+            'UPDATE users SET id = ?, username = ?, email = ? WHERE username = ?',
+            [id, username, email, before_username]
+        );
+
+        await connection.execute(
+            'UPDATE board SET username = ? WHERE username = ?',
+            [username, before_username]
+        );
+
+        const [user] = await connection.execute(
+            'SELECT * FROM users WHERE id = ?',
+            [id]
+        )
+
+        if (user) {
+            const jwt_token = await jwt.generate_jwt({ username : user[0].username, role : user[0].role});
+
+            return jwt_token;
+        }
+    } catch (e) {
+        console.log('change_info error : ' + e);
     } finally {
         connection.release();
     }
