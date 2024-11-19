6d022884979c071d96f229b041b6fb4d16cf2795
jiseoung <misojskim@naver.com>
2024-11-19 17:02:10 +0900

    feat: inc view if click write once a day

--- src/controllers/board.controllers/board_show.controllers.js
+++ src/controllers/board.controllers/board_show.controllers.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
+const cookieParser = require('cookie-parser');
 
 const board_show = require('../../services/board.service/board_show.service.js');
+const jwt = require('../../services/jwt.service.js');
+const manage_view = require('../../middlewares/board_manage_view.middleware.js');
 
 const router = express.Router();
 const app = express();
@@ -10,9 +12,18 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname + '/public/views'));
+router.use(cookieParser());
 
 router.get('/show', async (req, res) => {
     const { page } = req.query;
+    const jwt_token = req.cookies.jwt_token;
+    const username = await jwt.verify_jwt(jwt_token).username;
+
+    const is_viewed = await manage_view.viewed_before(page, username);
+
+    if (!is_viewed) {
+        await manage_view.increase_view(page, username);
+    }
 
     try {
         const show_write_result = await board_show.show_write(page);
