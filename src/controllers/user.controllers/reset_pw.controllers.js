f16d42f38fb6e54b6606739562dc3b41b865fa48
jiseoung <misojskim@naver.com>
2024-11-19 11:05:05 +0900

    feat: route reset pw

---
+++ src/controllers/user.controllers/reset_pw.controllers.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const user_pw = require('../../services/user.service/reset_pw.service.js');
+
+const router = express.Router();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set('views', path.join(__dirname + '/public/views'));
+
+router.get('/find_pw', (req, res) => {
+    res.render('find_pw');
+})
+
+router.post('/find_pw', async (req, res) => {
+    const { id, username, email } = req.body;
+
+    const result = await user_pw.reset_pw(id, username, email);
+
+    if (result === 'reset_complete') {
+        res.send('<script>alert("Password reset email has been sent.");location.href="/user/login";</script>');
+    }
+})
+
+module.exports = router;
