5dbffe6109be542a851d7d4f44211abc15635c81
jiseoung <misojskim@naver.com>
2024-11-22 02:06:53 +0900

    feat: route good and bad

--- src/controllers/board.controllers/board_show.controllers.js
+++ src/controllers/board.controllers/board_show.controllers.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const board_show = require('../../services/board.service/board_show.service.js');
 const jwt = require('../../services/jwt.service.js');
 const manage_view = require('../../middlewares/board_manage_view.middleware.js');
+const Good_Bad = require('../../services/board.service/board_good_bad.service.js');
 
 const router = express.Router();
 const app = express();
@@ -25,16 +26,35 @@ router.get('/show', async (req, res) => {
         await manage_view.increase_view(page, username);
     }
 
+    const good_and_bad = new Good_Bad(page, username);
+    const check_good_and_bad = await good_and_bad.click_check();
+
     try {
         const show_write_result = await board_show.show_write(page);
 
         if (show_write_result) {
-            res.render('board_show', { 'write' : show_write_result });
+            res.render('board_show', { 'write' : show_write_result, 'good_and_bad' : check_good_and_bad });
         }
     } catch (e) {
         console.log('/show error' + e);
     }
 });
+
+router.put('/show/good_bad', async (req, res) => {
+    const { page, click } = req.body;
+    const jwt_token = req.cookies.jwt_token;
+    const username = await jwt.verify_jwt(jwt_token).username;
+
+    try {
+        const good_and_bad = new Good_Bad(page, username);
+
+        await good_and_bad.click_reflection(click);
+    } catch (e) {
+        console.log('/show/good_bad error : ' + e);
+    }
+
+    res.send("ok");
+})
 
 router.post('/show/download', (req, res) => {
     const { file_name } = req.body;
