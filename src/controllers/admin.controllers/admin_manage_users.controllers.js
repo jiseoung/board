986ae91278b7e0694369a2a3283b7cc2335028f8
jiseoung <misojskim@naver.com>
2024-11-22 17:46:43 +0900

    feat: user delete router

--- src/controllers/admin.controllers/admin_manage_users.controllers.js
+++ src/controllers/admin.controllers/admin_manage_users.controllers.js
@@ -8,11 +8,20 @@ const router = express.Router();
 const app = express();
 
 app.set('views', path.join(__dirname + '/public/views'));
+app.use(express.json());
 
 router.get('/manage_users', check_admin, async (req, res) => {
     const user_list = await manage_users.user_list();
 
     res.render('admin_manage_users', { 'user_list' : user_list });
+})
+
+router.delete('/manage_users/delete', check_admin, async (req, res) => {
+    const { username } = req.body;
+
+    await manage_users.delete_user(username);
+
+    res.send('ok');
 })
 
 module.exports = router;
