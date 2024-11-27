4d2d57d722f5772dac2ecb9f12c1ae0a201d28e6
jiseoung <misojskim@naver.com>
2024-11-27 14:46:04 +0900

    delete

--- src/controllers/board.controllers/board_index.controllers.js
+++ src/controllers/board.controllers/board_index.controllers.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 
-const board_index = require('../../services/board.service/board_index.service.js');
 const jwt = require('../../services/jwt.service.js');
+const board_index = require('../../services/board.service/board_pagination.service.js');
 
 const app = express();
 const router = express.Router();
@@ -12,10 +12,11 @@ app.set('views', path.join(__dirname + '/public/views'));
 router.use(cookieParser());
 
 router.get('/', async (req, res) => {
+    const { page_index } = req.query;
     const user = await jwt.verify_jwt(req.cookies.jwt_token);
-    const writes_list = await board_index.show_writes_list();
+    const { total_page, writes_list } = await board_index.pagination(Number(page_index));
 
-    res.render('board_index', { 'writes_list' : writes_list, 'user' : user });
+    res.render('board_index', { 'total_page' : total_page, 'page_index' : page_index, 'writes_list' : writes_list, 'user' : user });
 });
 
 module.exports = router;
