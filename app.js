5ffe7abf9ac5b87c8332876d8d6d2be92f1b1234
jiseoung <misojskim@naver.com>
2024-11-27 12:56:45 +0900

    feat: route write delete

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const board_index_router = require(__dirname + '/src/controllers/board.controlle
 const board_write_router = require(__dirname + '/src/controllers/board.controllers/board_write.controllers.js');
 const board_show_router = require(__dirname + '/src/controllers/board.controllers/board_show.controllers.js');
 const board_modify_router = require(__dirname + '/src/controllers/board.controllers/board_modify.controllers.js');
+const board_delete_router = require(__dirname + '/src/controllers/board.controllers/board_delete.controllers.js');
 
 const admin_index_router = require(__dirname + '/src/controllers/admin.controllers/admin_index.controllers.js');
 const admin_manage_users_router = require(__dirname + '/src/controllers/admin.controllers/admin_manage_users.controllers.js');
@@ -35,6 +36,7 @@ app.use("/public/js", express.static(path.join(__dirname + "/public/js" )));
 app.use("/public/css", express.static(path.join(__dirname + "/public/css" )));
 
 app.use('/', index_router);
+
 app.use('/user', register_router);
 app.use('/user', login_router);
 app.use('/user', logout_router);
@@ -42,10 +44,13 @@ app.use('/user', info_router);
 app.use('/user', change_info_router);
 app.use('/user', change_pw_router);
 app.use('/user', reset_pw_router);
+
 app.use('/board', board_index_router);
 app.use('/board', board_write_router);
 app.use('/board', board_show_router);
 app.use('/board', board_modify_router);
+app.use('/board', board_delete_router);
+
 app.use('/admin', admin_index_router);
 app.use('/admin', admin_manage_users_router);
 app.use('/admin', admin_reported_writes_router);
