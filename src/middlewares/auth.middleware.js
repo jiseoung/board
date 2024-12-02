ade4ca658388ee58e459ed68569ef6012e746219
jiseoung <misojskim@naver.com>
2024-12-02 19:41:53 +0900

    feat: check login

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,24 @@
+const jwt = require('../services/jwt.service.js');
+
+const auth = async (req, res, next) => {
+    const jwt_token = req.cookies.jwt_token;
+
+    if (jwt_token === undefined) {
+        res.send('<script>alert("login first"); location.href="/user/login"; </script>');
+    }
+
+    try {
+        const user = await jwt.verify_jwt(jwt_token);
+
+        if (user === 'expired') {
+            res.send('<script>alert("login again"); location.href="/user/login"; </script>');
+        }
+        else {
+            next();
+        }
+    } catch (e) {
+        console.log('auth error : ' + e);
+    }
+}
+
+module.exports = auth;
