74833da7acfac94cd92c74676d67ed28b3df7c8f
jiseoung <misojskim@naver.com>
2024-12-02 17:40:49 +0900

    feat: organize user controllers

---
+++ src/routes/user.router.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const router = express.Router();
+
+const register_controllers = require('../controllers/user.controllers/register.controllers.js');
+const login_controllers = require('../controllers/user.controllers/login.controllers.js');
+const logout_controllers = require('../controllers/user.controllers/logout.controllers.js');
+const info_controllers = require('../controllers/user.controllers/info.controllers.js');
+const change_info_controllers = require('../controllers/user.controllers/change_info.controllers.js');
+const change_pw_controllers = require('../controllers/user.controllers/change_pw.controllers.js');
+const reset_pw_controllers = require('../controllers/user.controllers/reset_pw.controllers.js');
+
+router.use('/user', register_controllers);
+router.use('/user', login_controllers);
+router.use('/user', logout_controllers);
+router.use('/user', info_controllers);
+router.use('/user', change_info_controllers);
+router.use('/user', change_pw_controllers);
+router.use('/user', reset_pw_controllers);
+
+module.exports = router;
