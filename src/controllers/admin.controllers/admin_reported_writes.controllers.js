760930173aaf872218b3875c8de15d263b0a266b
jiseoung <misojskim@naver.com>
2024-12-02 19:41:43 +0900

    chore: delete check admin middleware

--- src/controllers/admin.controllers/admin_reported_writes.controllers.js
+++ src/controllers/admin.controllers/admin_reported_writes.controllers.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 
-const check_admin = require('../../middlewares/check_admin.middleware.js');
 const reported_writes = require('../../services/admin.service/reported_writes.service.js');
 
 const router = express.Router();
@@ -9,7 +8,7 @@ const app = express();
 
 app.set('views', path.join(__dirname + '/public/views'));
 
-router.get('/reported_writes', check_admin, async (req, res) => {
+router.get('/reported_writes', async (req, res) => {
     const reported_writes_list = await reported_writes.list();
 
     res.render('admin_reported_writes', { 'reported_writes_list' : reported_writes_list });
