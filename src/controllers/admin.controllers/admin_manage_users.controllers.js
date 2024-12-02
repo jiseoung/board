9863856cb6b99b160f12912b2e648bfde11f8985
jiseoung <misojskim@naver.com>
2024-12-02 19:41:23 +0900

    chore: delete check admin middleware

--- src/controllers/admin.controllers/admin_manage_users.controllers.js
+++ src/controllers/admin.controllers/admin_manage_users.controllers.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 
-const check_admin = require('../../middlewares/check_admin.middleware.js');
 const manage_users = require("../../services/admin.service/manage_users.service.js");
 
 const router = express.Router();
@@ -10,13 +9,13 @@ const app = express();
 app.set('views', path.join(__dirname + '/public/views'));
 app.use(express.json());
 
-router.get('/manage_users', check_admin, async (req, res) => {
+router.get('/manage_users', async (req, res) => {
     const user_list = await manage_users.user_list();
 
     res.render('admin_manage_users', { 'user_list' : user_list });
 })
 
-router.delete('/manage_users/delete', check_admin, async (req, res) => {
+router.delete('/manage_users/delete', async (req, res) => {
     const { username } = req.body;
 
     await manage_users.delete_user(username);
