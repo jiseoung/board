bfcfa14d3f484eb5d1faa1be36910c1eda927c6d
jiseoung <misojskim@naver.com>
2024-12-02 19:42:14 +0900

    feat: check login and admin

--- src/routes/admin.router.js
+++ src/routes/admin.router.js
@@ -6,8 +6,11 @@ const admin_index_controllers = require('../controllers/admin.controllers/admin_
 const admin_manage_users_controllers = require('../controllers/admin.controllers/admin_manage_users.controllers.js');
 const admin_reported_writes_controllers = require('../controllers/admin.controllers/admin_reported_writes.controllers.js');
 
-router.use('/admin', admin_index_controllers);
-router.use('/admin', admin_manage_users_controllers);
-router.use('/admin', admin_reported_writes_controllers);
+const auth = require('../middlewares/auth.middleware.js');
+const check_admin = require('../middlewares/check_admin.middleware.js');
+
+router.use('/admin', auth, check_admin, admin_index_controllers);
+router.use('/admin', auth, check_admin, admin_manage_users_controllers);
+router.use('/admin', auth, check_admin, admin_reported_writes_controllers);
 
 module.exports = router;
