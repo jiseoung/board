faa33ad70e99c822a5a6a401382dffda0f8b4c45
jiseoung <misojskim@naver.com>
2024-12-03 02:08:34 +0900

    feat: Handle comment edit/delete permission

--- src/controllers/board.controllers/board_comment.controllers.js
+++ src/controllers/board.controllers/board_comment.controllers.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const jwt = require('../../services/jwt.service.js');
 const board_comment = require('../../services/board.service/board_comment.service.js');
@@ -9,11 +10,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.set('views', path.join(__dirname + '/public/views'));
 router.use(cookieParser());
 
 router.post('/show/comment', async (req, res) => {
     const { page, comment, secret } = req.body;
     const username = await jwt.verify_jwt(req.cookies.jwt_token).username;
+
     const secret_check = await board_comment.secret_check(secret);
 
     await board_comment.leave_comment(page, username, comment, secret_check);
@@ -24,6 +27,7 @@ router.post('/show/comment', async (req, res) => {
 router.post('/show/reply', async (req, res) => {
     const { page, parent_index, reply, secret } = req.body;
     const username = await jwt.verify_jwt(req.cookies.jwt_token).username;
+
     const secret_check = await board_comment.secret_check(secret);
 
     try {
@@ -38,13 +42,19 @@ router.post('/show/reply', async (req, res) => {
 
 router.post('/show/comment/edit', async (req, res) => {
     const { page, com_index, comment_edit, secret } = req.body;
+    const { username, role } = await jwt.verify_jwt(req.cookies.jwt_token);
+
     const secret_check = await board_comment.secret_check(secret);
 
     try {
-        await board_comment.edit_comment(com_index, comment_edit, secret_check);
+        const result = await board_comment.edit_comment(username, role, com_index, comment_edit, secret_check);
+
+        if (result === 'fail') {
+            res.send('<script>alert("you cant edit"); location.href="/board/show?page=' + page + '"</script>');
+        }
     } catch (e) {
         console.log('/show/comment error : ' + e);
-        res.render(400);
+        return res.status(400).render(400);
     }
 
     res.send('<script>location.href="/board/show?page=' + page + '"</script>');
@@ -52,15 +62,21 @@ router.post('/show/comment/edit', async (req, res) => {
 
 router.delete('/show/comment', async (req, res) => {
     const { com_index } = req.body;
+    const { username, role } = await jwt.verify_jwt(req.cookies.jwt_token);
 
     try {
-        await board_comment.delete_comment(com_index);
+        const result = await board_comment.delete_comment(username, role, com_index);
+
+        if(result === 'fail') {
+            res.status(400).send('400 Bad Request');
+        }
+        else if (result === 'succeed') {
+            res.status(200).send('ok');
+        }
     } catch (e) {
         console.log('delete /show/comment error : ' + e);
-        res.render(400);
+        res.status(400).render(400);
     }
-
-    res.send('ok');
 })
 
 module.exports = router;
