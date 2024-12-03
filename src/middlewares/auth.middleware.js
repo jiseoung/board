49159a85bbbdb3300a0968a2ee9752a7a7219f7e
jiseoung <misojskim@naver.com>
2024-12-03 10:49:58 +0900

    feat: add status

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,14 +4,14 @@ const auth = async (req, res, next) => {
     const jwt_token = req.cookies.jwt_token;
 
     if (jwt_token === undefined) {
-        res.send('<script>alert("login first"); location.href="/user/login"; </script>');
+        res.status(400).send('<script>alert("login first"); location.href="/user/login"; </script>');
     }
 
     try {
         const user = await jwt.verify_jwt(jwt_token);
 
         if (user === 'expired') {
-            res.send('<script>alert("login again"); location.href="/user/login"; </script>');
+            res.status(400).send('<script>alert("login again"); location.href="/user/login"; </script>');
         }
         else {
             next();
