f093f92d85d6dd044518a391e237bb811f62168f
jiseoung <misojskim@naver.com>
2024-12-03 10:49:18 +0900

    feat: auth check

--- src/controllers/board.controllers/board_delete.controllers.js
+++ src/controllers/board.controllers/board_delete.controllers.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const jwt = require("../../services/jwt.service.js");
 const board_delete = require('../../services/board.service/board_delete.service.js');
 
 const router = express.Router();
@@ -9,10 +10,18 @@ app.use(express.json());
 
 router.delete('/delete', async (req, res) => {
     const { page } = req.body;
+    const { username, role } = await jwt.verify_jwt(req.cookies.jwt_token);
     
-    await board_delete.delete_write(page);
+    const result = await board_delete.delete_write(page, username, role);
 
-    res.send('ok');
+    if (result === 'succeed') {
+        res.status(200).send('ok');
+    }
+    else {
+        res.status(400).send('Bad Request');
+    }
+
+
 })
 
 module.exports = router;
